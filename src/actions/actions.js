fb02d5c77d61edbc7b35025e11d301ad5e5e54b4
BrownCow371 <jmbrowncow@gmail.com>
2018-10-27 18:47:15 -0700

    update actions to handle fetching current weather AND forecast weather from api

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,6 +5,17 @@ export function fetchWeather() {
         // look into how to pass multiple search params other than zip, like units
     return fetch('/api/weather')
         .then(response => response.json())
-        .then(weather => dispatch({type: 'FETCH_WEATHER', payload: weather.list}))
+        .then(weather => dispatch({type: 'FETCH_WEATHER', payload: weather}))
+    }
+}
+
+export function fetchForecast() {
+    // will need a n argument in future to grab zip code and preferences
+    return (dispatch) => {
+        dispatch({type: 'LOADING_WEATHER'});
+        // look into how to pass multiple search params other than zip, like units
+    return fetch('/api/forecast')
+        .then(response => response.json())
+        .then(forecast => dispatch({type: 'FETCH_FORECAST', payload: forecast.list}))
     }
 }
