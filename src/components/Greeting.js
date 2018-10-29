c74f14a38f3893dff837e51071d4dfe5ae0637c6
BrownCow371 <jmbrowncow@gmail.com>
2018-10-29 14:39:39 -0700

    add weather search component to Greeting component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import WeatherSearch from './WeatherSearch.js';
+import { Link } from 'react-router-dom';
+
+{/* <Link to = "/weather" exact>Weather Search</Link> */}
 
 class Greeting extends Component {
 
     render(){
         return(
-            <h1>Welcome to the Weather Based Activity Suggestion App!</h1>
+            <div>
+                <h1>Welcome to the Weather Based Activity Suggestion App!</h1>
+                <WeatherSearch />
+            </div>
          )
     }
 
