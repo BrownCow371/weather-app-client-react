5de0d5f3941e8fad7d4b14cc279c203777f1b1d8
BrownCow371 <jmbrowncow@gmail.com>
2018-10-29 14:43:39 -0700

    add zipcode entry validation and disable submit button if not valid. add handleOnChange function for potential future route support

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -1,31 +1,46 @@
-import React, {Component} from 'react';
+import React, {Component, Redirect} from 'react';
 import {connect} from 'react-redux';
 import {fetchWeather, fetchForecast} from '../actions/actions.js';
+import OneDayWeather from './OneDayWeather.js';
+
 
 
 class WeatherSearch extends Component {
     constructor(props){
         super(props);
         this.state ={
-            zip: '10004'
+            zip: '10004',
         }
+    }
+
+    handleOnChange = (event) => {
+        this.setState({zip: event.target.value})
     }
 
     onSubmitHandler = (event) => {
         event.preventDefault();
-        // this.props.fetchWeather(this.state.zip);
-        // this.props.fetchForecast(this.state.zip);
+        this.props.fetchWeather(this.state.zip)
     }
 
     render(){
+
+        const isEnabled = this.state.zip.match(/\b\d{5}\b/)
+
         return(
-            <form onSubmit={this.onSubmitHandler}>
-            <label> Enter Zipcode for Local Weather: </label>
-            <input type="text" inChange={event =>this.setState({zip: event.target.value})} value={this.state.zip}/>
-            <input type="submit" />
-            </form>
+            <>
+                <form onSubmit={this.onSubmitHandler}>
+                <label> Enter Zipcode for Local Weather: </label>
+                <input type="text" onChange={this.handleOnChange} value={this.state.zip}/>
+                <input type="submit" disabled={!isEnabled} />
+                </form>
+                <p>Only 5 digit US ZipCodes accepted. </p>
+            </>
         )
     }
 }
+
+const mapStoreToProps = (state) => {
+    return {weather: state.weather}
+  }
 
 export default connect(null,{fetchWeather})(WeatherSearch)
