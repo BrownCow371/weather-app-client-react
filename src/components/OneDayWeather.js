8c47459c94bfce00421bcfdc48b50c8b7c625437
BrownCow371 <jmbrowncow@gmail.com>
2018-10-29 09:39:02 -0700

    try to get store to work

--- src/components/OneDayWeather.js
+++ src/components/OneDayWeather.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
 
-export const OneDayWeather = (props) => {
-    return (
-        <ul>
+class OneDayWeather extends Component {
 
-        </ul> 
-    )
+
+    render() {
+        // let iconUrl = this.props.weather.map(weather => `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`)[0]
+
+        return (
+            <>
+                <h2>Current Weather</h2>
+                    
+            {this.props.weather.map((weather, index) => (
+                <ul key={index}>    
+                     <li >{weather.main.temp}</li>
+                     <li>{weather.weather[0].main}</li>
+                     <li><img src={"http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"} alt="weather icon"/></li>
+                 </ul> ))}
+            </>
+        )
+    }
 }
+
+// const mapStoreToProps = (state) => {
+//     return {weather: state.weather}
+//   }
+
+//   export default connect(mapStoreToProps)(OneDayWeather)
+export default OneDayWeather;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import {connect} from 'react-redux';
-import {fetchWeather} from '../actions/actions.js';
+import {fetchWeather, fetchForecast} from '../actions/actions.js';
 import FiveDayWeather from '../components/FiveDayWeather.js';
+import OneDayWeather from '../components/OneDayWeather.js';
+import Greeting from '../components/Greeting.js';
+import WeatherSearch from '../components/WeatherSearch.js';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
 
-  componentDidMount(){
-    //fetch current weather
-    this.props.fetchWeather();
-  }
+  // componentDidMount(){
+  //   //fetch current weather
+  //   this.props.fetchForecast('10004');
+  //   this.props.fetchWeather('10004');
+  // }
+
+  // <FiveDayWeather weather={this.props.weather} />
+  // {this.props.weather.main.temp}
+  // {this.props.forecast.map(forecast => <li>{forecast.dt_txt}</li>)}
 
   render() {
     // const {weather} = this.props
     console.log("weather", this.props.weather)
+    console.log("forecast", this.props.forecast)
+
     // debugger;
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>Does this really work</p>
-          <FiveDayWeather weather={this.props.weather} />
-          
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+            <Greeting />
+            <WeatherSearch />
+              <Route exact path="/" component={Greeting}/>
+              <Route exact path='/weather' component={OneDayWeather} />
+              <Route exact path='/forecast' component={FiveDayWeather} />
+            <OneDayWeather weather={this.props.weather}/>
+        </div>
+      </Router>
     );
   }
 }
 const mapStoreToProps = (state) => {
-  return {weather: state.weather}
+  return {weather: state.weather,
+          forecast:state.forecast}
 }
 
-export default connect(mapStoreToProps, {fetchWeather})(App);
+export default connect(mapStoreToProps, {fetchWeather, fetchForecast})(App);
