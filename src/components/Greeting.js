cd0106474caed82db039e157ad307ba528379707
BrownCow371 <jmbrowncow@gmail.com>
2018-10-29 09:35:40 -0700

    add Greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Greeting extends Component {
+
+    render(){
+        return(
+            <h1>Welcome to the Weather Based Activity Suggestion App!</h1>
+         )
+    }
+
+}
+
+export default Greeting
