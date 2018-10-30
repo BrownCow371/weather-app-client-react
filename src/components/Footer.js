3edf59bbf4eca7b3577da9181a9d3e8b09d81668
BrownCow371 <jmbrowncow@gmail.com>
2018-10-30 14:15:52 -0700

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+    <footer class="branding">
+        <small>&copy; 2018 <strong>Weather Activity App</strong></small>
+        <small> - Dog image provided by Pixel</small>
+    </footer>)
+}
+
+export default Footer;
