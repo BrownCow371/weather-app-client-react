9987cb45599d5ccd25821d36a677e0d1ed634065
BrownCow371 <jmbrowncow@gmail.com>
2018-10-30 14:20:05 -0700

    add zip argument to weather and forecast functions. add fetch activities function

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,21 +1,31 @@
-export function fetchWeather() {
+export function fetchWeather(zip) {
     // will need a n argument in future to grab zip code and preferences
     return (dispatch) => {
-        dispatch({type: 'LOADING_WEATHER'});
+        dispatch({type: 'LOADING_DATA'});
         // look into how to pass multiple search params other than zip, like units
-    return fetch('/api/weather')
-        .then(response => response.json())
-        .then(weather => dispatch({type: 'FETCH_WEATHER', payload: weather}))
+        return fetch(`/api/weather/${zip}`)
+            .then(response => response.json())
+            .then(weather => dispatch({type: 'FETCH_WEATHER', payload: weather}))
     }
 }
 
-export function fetchForecast() {
+export function fetchForecast(zip) {
     // will need a n argument in future to grab zip code and preferences
     return (dispatch) => {
-        dispatch({type: 'LOADING_WEATHER'});
+        dispatch({type: 'LOADING_DATA'});
         // look into how to pass multiple search params other than zip, like units
-    return fetch('/api/forecast')
-        .then(response => response.json())
-        .then(forecast => dispatch({type: 'FETCH_FORECAST', payload: forecast.list}))
+        return fetch(`/api/forecast/${zip}`)
+            .then(response => response.json())
+            .then(forecast => dispatch({type: 'FETCH_FORECAST', payload: forecast.list}))
+    }
+}
+
+export function fetchActivities() {
+    return (dispatch) => {
+       dispatch({type: 'LOADING_DATA'});
+       return fetch('/api/activities')
+            .then(response => response.json())
+            .then(activities => dispatch({type: 'FETCH_ACTIVITIES', payload: activities}))
+
     }
 }
