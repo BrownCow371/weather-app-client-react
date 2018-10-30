ccf996062ba27aaf4e6a9e489110322bbd0c483b
BrownCow371 <jmbrowncow@gmail.com>
2018-10-30 14:27:11 -0700

    change OneDayWeather from class to a function. add internal function for conditional rendering. add conditional rendering.

--- src/components/OneDayWeather.js
+++ src/components/OneDayWeather.js
@@ -1,28 +1,42 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import React from 'react';
+// import {connect} from 'react-redux'
 
-class OneDayWeather extends Component {
+const OneDayWeather = (props) => {
 
-    render() {
-        console.log("weather", this.props.weather)
+        function renderThis(){
+            console.log("weather", props.weather)
+
+            return !!props.weather[0]
+        }
+  
+
+        if (renderThis()) {
         return (
             <>
-            <h2>Current Weather</h2>
+            <h2 className="center">Current Weather for {props.weather[0].zip}:</h2>
                     
-            {this.props.weather.map((weather) => (
-                <div key={weather.id}>    
+            {props.weather.map((weather) => (
+                <div key={weather.id} className="weather-box">    
                      <p><strong>Temperature:</strong> {weather.temp}</p>
-                     <p><strong>Conditions:</strong> {weather.main}</p>
-                     <p><img src={"http://openweathermap.org/img/w/" + weather.icon + ".png"} alt="weather icon"/></p>
+                     <p><strong>Conditions:</strong> {weather.desc}</p>
+                     <p><strong>Humidity:</strong> {weather.humidity}</p>
+                     <p><strong>Wind Speed:</strong> {weather.wind_speed}</p>
+                     <p className="center"><img src={"http://openweathermap.org/img/w/" + weather.icon + ".png"} alt="weather icon"/></p>
                  </div> ))}
             </>
         )
-    }
+        } else {
+                return (
+                    <>
+                    </>
+                )
+            }
+   
 }
 
-const mapStoreToProps = (state) => {
-    return {weather: state.weather}
-  }
+// const mapStoreToProps = (state) => {
+//     return {weather: state.weather}
+//   }
 
-  export default connect(mapStoreToProps)(OneDayWeather)
-// export default OneDayWeather;
+//   export default connect(mapStoreToProps)(OneDayWeather)
+export default OneDayWeather;
