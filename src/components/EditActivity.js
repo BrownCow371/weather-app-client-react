ba51e3c81bccf3cbdb020ebf3b7769b39d1c332b
BrownCow371 <jmbrowncow@gmail.com>
2018-10-31 14:54:14 -0700

    set up constructor. add fetch calls directly to componentDidMOunt instead of using store since need to set internal state. build out form to autofill with state. include all condition check boxes.

--- src/components/EditActivity.js
+++ src/components/EditActivity.js
@@ -1,36 +1,73 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchActivity} from '../actions/actions.js';
+import {fetchActivity, updateActivity} from '../actions/actions.js';
 
 class EditActivity extends Component {
-    // constructor(props){
-    //     super(props)
-
-    //     let activityId = this.props.match.params.id
-    //     let activity = this.props.activities.find(activity => activity.id === activityId)
-    //     debugger;
-    //     this.state ={
-    //         desc: activity.desc,
-    //         min_temp: activity.min_temp,
-    //         max_temp: activity.max_temp,
-    //         max_wind_speed: activity.max_wind_speed
-    //     }
-    // }
-
-    componentDidMount(){
-        this.props.fetchActivity(this.props.match.params.id)
+    constructor(props){
+        super(props)
+        this.state = {
+            activity: {
+                id: '',
+                desc: '',
+                min_temp: '',
+                max_temp: '',
+                max_wind_speed: '',
+                conditions: []
+            },
+            conditions: []
+        }
     }
 
-    render(){
+    componentDidMount(){
+        let id = this.props.match.params.id;
+        fetch(`/api/activities/${id}`)
+            .then(response => response.json())
+            .then(activity => this.setState({activity: activity}))
+        fetch(`/api/conditions`)
+            .then(response => response.json())
+            .then(conditions => this.setState({conditions: conditions}))
+    }
 
+   
+    handleChange = (event) => {
+        this.setState({
+            activity: {...this.state.activity, [event.target.name]: event.target.value }
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.updateActivity();
+    }
+ 
+
+    render(){
+        // debugger;
         return (
-            <p>I am an Edit page for activity ID {this.props.match.params.id} </p>
-        )
+            <div className="activity-box">
+                <form onSubmit={this.handleSubmit}> 
+                    <label><strong>Description: </strong></label><input type ="text" name="desc" value={this.state.activity.desc} onChange={this.handleChange} /><br/>
+                    <label><strong>Min Temp: </strong></label><input type ="text" name="min_temp" value={this.state.activity.min_temp} onChange={this.handleChange} /><br/>
+                    <label><strong>Max Temp: </strong></label><input type ="text" name="max_temp" value={this.state.activity.max_temp} onChange={this.handleChange} /><br/>
+                    <label><strong>Max Wind Speed: </strong></label><input type ="text" name="max_wind_speed" value={this.state.activity.max_wind_speed} onChange={this.handleChange} /><br/>
+                    <label><strong>Acceptable Conditions: </strong></label>
+                        {this.state.conditions.map(condition => (
+                            <label><input key={condition.id} 
+                            type="checkbox" 
+                            value={condition.id} 
+                            name="condition" 
+                            checked={!!this.state.activity.conditions.find(c => c.id ===condition.id)}
+                            id={"condition_id_"+condition.id}/>{condition.desc    } </label>
+                        ))} <br/>
+                    <input className="big-button center" type="submit" />
+                </form>
+            </div>
+       )
     }
 }
 
-const mapStateToProps = (state) => {
-    return {activities: state.activities}
-  }
+// const mapStateToProps = (state) => {
+//     return {activity: state.activity}
+//   }
 
-export default connect(mapStateToProps, {fetchActivity})(EditActivity)
+export default connect(null, {updateActivity})(EditActivity)
