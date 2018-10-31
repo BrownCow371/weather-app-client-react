4bc918079967980c6542135d320edcf027f55da8
BrownCow371 <jmbrowncow@gmail.com>
2018-10-31 10:21:05 -0700

    update class to className on footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-    <footer class="branding">
+    <footer className="branding">
         <small>&copy; 2018 <strong>Weather Activity App</strong></small>
         <small> - Dog image provided by Pixel</small>
     </footer>)
