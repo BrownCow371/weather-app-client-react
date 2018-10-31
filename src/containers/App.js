f5d9bcf4f184dd822a4d20bf9a675e23b35a3773
BrownCow371 <jmbrowncow@gmail.com>
2018-10-31 10:24:26 -0700

    add route for Edit Activity

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,7 @@ import NavBar from '../components/NavBar.js'
 import Footer from '../components/Footer.js'
 import FiveDayWeather from '../components/FiveDayWeather.js';
 import Greeting from '../components/Greeting.js';
+import EditActivity from '../components/EditActivity.js'
 
 // other containers
 import Activities from './Activities.js';
@@ -17,6 +18,8 @@ class App extends Component {
 
    render() {
 
+    // <Route exact path='/activities/:id/edit' render={routerProps => <EditActivity {...routerProps} /> } />
+
     return (
       <Router>
         <div className="wrapper">
@@ -25,6 +28,7 @@ class App extends Component {
               <Route exact path='/weather' component={WeatherSearch} />
               <Route exact path='/forecast' component={FiveDayWeather} />
               <Route exact path='/activities' component={Activities} />
+              <Route exact path='/activities/:id/edit' component={EditActivity} />
               <Footer />
         </div>
       </Router>
