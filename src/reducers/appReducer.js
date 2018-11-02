05d075cc47659b0d8e5386b32a5a449ca93670e3
BrownCow371 <jmbrowncow@gmail.com>
2018-11-01 19:00:01 -0700

    remove unneeed imports from reducer. update weather attribute in state to be a hash instead of an array. remove fetch forecast from reducer. add laoding:false to update activity. add an add activity case

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,13 +1,10 @@
-// import {defaultWeatherState} from './defaultState.js'
-// import {defaultForecastState} from './defaultState.js'
 
 export default function appReducer(
     state = {
         loading: false,
-        weather: [],
+        weather: {},
         conditions: [],
         activities: [],
-        activity: {}
        }, 
     action){
 
@@ -15,18 +12,20 @@ export default function appReducer(
         case 'LOADING_DATA':
             return {...state, loading: true}
         case 'FETCH_WEATHER':
-            return {...state, weather: [action.payload], loading: false}
-        case 'FETCH_FORECAST':
-            return {...state, forecast: action.payload, loading:false}
+            return {...state, weather: action.payload, loading: false}
+        // case 'FETCH_FORECAST':
+        //     return {...state, forecast: action.payload, loading:false}
         case 'FETCH_ACTIVITIES':
             return {...state, activities: action.payload, loading: false}
         case 'FETCH_CONDITIONS':
             return {...state, conditions: action.payload, loading: false}
         case 'UPDATE_ACTIVITY':
-            return {...state, activities: state.activities.map(act => (
-                act.id === action.payload.id ? action.payload : act)) }
+            return {...state, activities: state.activities.map(act => 
+                (act.id === action.payload.id ? action.payload : act)), loading: false }
+        case 'ADD_ACTIVITY':
+            return {...state, activities: [...state.activities, action.payload], loading: false }
         default:
-        return state;
+            return state;
     }
 
 }
