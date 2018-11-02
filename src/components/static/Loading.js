07c1b1695c5cf09e814d6d093caef52bc522a7e0
BrownCow371 <jmbrowncow@gmail.com>
2018-11-02 10:41:04 -0700

    add static loading component

---
+++ src/components/static/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loading = () => {
+
+    return (
+            <h1 className="warning"> Please Hold....Loading Data </h1>
+           )
+}
+
+export default Loading;
