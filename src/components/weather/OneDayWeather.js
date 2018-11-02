f334a23a34a56cddf6ca7c082942185f4b9ca40b
BrownCow371 <jmbrowncow@gmail.com>
2018-11-01 19:22:03 -0700

    add object destructuring in funtion arguments. add comments. add conditional rending based on loading prop.

--- src/components/weather/OneDayWeather.js
+++ src/components/weather/OneDayWeather.js
@@ -1,42 +1,34 @@
 import React from 'react';
-// import {connect} from 'react-redux'
 
-const OneDayWeather = (props) => {
+const OneDayWeather = ({weather, loading}) => {
 
+
+        //Determine if weather has been fetched yet.
         function renderThis(){
-            console.log("weather", props.weather)
-
-            return !!props.weather[0]
+            return !!weather.zip
         }
   
-
+        //if weather has been fetched, render the weather box. if not, render null 
         if (renderThis()) {
-        return (
-            <>
-            <h2 className="center">Current Weather for {props.weather[0].zip}:</h2>
-                    
-            {props.weather.map((weather) => (
-                <div key={weather.id} className="weather-box">    
-                     <p><strong>Temperature:</strong> {weather.temp}</p>
-                     <p><strong>Conditions:</strong> {weather.desc}</p>
-                     <p><strong>Humidity:</strong> {weather.humidity}</p>
-                     <p><strong>Wind Speed:</strong> {weather.wind_speed}</p>
-                     <p className="center"><img src={"http://openweathermap.org/img/w/" + weather.icon + ".png"} alt="weather icon"/></p>
-                 </div> ))}
-            </>
-        )
+            return (
+                <>
+                    <h2 className="center">Current Weather for {weather.zip}:</h2>     
+                    <div key={weather.id} className="weather-box">    
+                        <p><strong>Temperature:</strong> {weather.temp}</p>
+                        <p><strong>Conditions:</strong> {weather.desc}</p>
+                        <p><strong>Humidity:</strong> {weather.humidity}</p>
+                        <p><strong>Wind Speed:</strong> {weather.wind_speed}</p>
+                        <p className="center"><img src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt="weather icon"/></p>
+                    </div> 
+                </>
+            )
+        } else if (loading) {
+            return <div className="weather-box"> <h3>LOADING DATA</h3> </div>
+            
         } else {
-                return (
-                    <>
-                    </>
-                )
-            }
+            return null;
+        }
    
 }
 
-// const mapStoreToProps = (state) => {
-//     return {weather: state.weather}
-//   }
-
-//   export default connect(mapStoreToProps)(OneDayWeather)
 export default OneDayWeather;
