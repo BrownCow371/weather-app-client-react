da4da8c8b79e60e319af699f36ec9089ed707d1d
BrownCow371 <jmbrowncow@gmail.com>
2018-11-01 19:18:02 -0700

    update variable names and fix bug with unchecking boxes

--- src/components/activity/changeFunctions.js
+++ src/components/activity/changeFunctions.js
@@ -6,17 +6,16 @@ export function handleActivityChange(event){
 }
 
 export function handleConditionSelection(event) {
-    const newSelection = this.props.conditions.find(c=> c.id == event.target.value);
-
-    let newSelectionArray;
+    const clickedCondition = this.props.conditions.find(c=> c.id == event.target.value);
+    let conditionArray;
     // if the id provided by the selection is found in the state array of activity conditions
     // then remove the condition from the state array.
     // else add the selection to the state array.
-    if(this.state.activity.conditions.findIndex(c => c.id == newSelection.id) > -1) {
-        newSelectionArray = this.state.activity.conditions.filter(c => c.id != newSelection)
+    if(this.state.activity.conditions.findIndex(c => c.id == clickedCondition.id) > -1) {
+        conditionArray = this.state.activity.conditions.filter(c => c.id != clickedCondition.id)
     } else {
-        newSelectionArray = [...this.state.activity.conditions, newSelection] 
+        conditionArray = [...this.state.activity.conditions, clickedCondition] 
     }
     
-    this.setState({activity: {...this.state.activity, conditions: newSelectionArray}})
+    this.setState({activity: {...this.state.activity, conditions: conditionArray}})
 }
