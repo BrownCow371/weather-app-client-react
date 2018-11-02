7410cf7a102e0d78afd203cfa2641617d7e94fc5
BrownCow371 <jmbrowncow@gmail.com>
2018-11-02 15:02:00 -0700

    fix type in footer

--- src/components/static/Footer.js
+++ src/components/static/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
     <footer className="branding">
         <small>&copy; 2018 <strong>Weather Activity App</strong></small>
-        <small> ---- Images provided by Pexel ---</small>
-        <small> ---- Weather data provided by OPenWeatherMap API ---</small>
+        <small> ---- Images provided by Pexel</small>
+        <small> ---- Weather data provided by OpenWeatherMap API ----</small>
     </footer>)
 }
 
