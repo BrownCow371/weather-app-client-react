62dc9d4263d4c565551e92d9a22d8fee38b86e42
BrownCow371 <jmbrowncow@gmail.com>
2018-11-02 11:15:03 -0700

    add validation function. update activitychange function to call validation function for each update in input. update ConditionsSelection function to parce ids to integers so can use exact match

--- src/components/activity/ActivityForm.js
+++ src/components/activity/ActivityForm.js
@@ -1,51 +1,61 @@
 import React from 'react';
+import ActivityFormErrors from './ActivityFormErrors.js'
 
-const ActivityForm = (props) => {
+const ActivityForm = ({formErrors, title, activity, conditions, handleChange, handleCheckbox, handleSubmit}) => {
 
-       
+    let isDisabled = ((Object.keys(formErrors).find(field => formErrors[field].length > 0)) || activity.desc === "")
+            //  debugger;  
     return (
-        <form onSubmit={props.handleSubmit}> 
-            <h2>{props.title}</h2>
+    <>
+        
+        <ActivityFormErrors formErrors={formErrors}/>
+        <form onSubmit={handleSubmit}> 
+            <h2>{title}</h2>
             <label><strong>Description: </strong></label>
                 <input 
                     type ="text" 
                     name="desc" 
-                    value={props.activity.desc} 
-                    onChange={props.handleChange} /><br/>
+                    value={activity.desc} 
+                    placeholder="Description Required"
+                    onChange={handleChange} /><br/>
             <label><strong>Min Temp: </strong></label>
                 <input 
                     type ="text" 
                     name="min_temp" 
-                    value={props.activity.min_temp} 
-                    onChange={props.handleChange} /><br/>
+                    value={activity.min_temp}
+                    placeholder="Optional"
+                    onChange={handleChange} /><br/>
             <label><strong>Max Temp: </strong></label>
                 <input 
                     type ="text" 
                     name="max_temp" 
-                    value={props.activity.max_temp} 
-                    onChange={props.handleChange} /><br/>
+                    value={activity.max_temp}
+                    placeholder="Optional"
+                    onChange={handleChange} /><br/>
             <label><strong>Max Wind Speed: </strong></label>
                 <input 
                     type ="text" 
                     name="max_wind_speed" 
-                    value={props.activity.max_wind_speed} 
-                    onChange={props.handleChange} /><br/>
+                    value={activity.max_wind_speed}
+                    placeholder="Optional"
+                    onChange={handleChange} /><br/>
             <label><strong>Acceptable Conditions: </strong></label>
                 <div className="checkbox"> 
-                    {props.conditions.map(condition => (
+                    {conditions.map(condition => (
                         <label key ={condition.id}> 
                             <input 
                                 type="checkbox"
                                 name="condition"
                                 value={condition.id}
-                                onChange={props.handleCheckbox}
-                                checked={props.activity.conditions.findIndex(c=> c.id == condition.id) > -1}
+                                onChange={handleCheckbox}
+                                checked={activity.conditions.findIndex(c=> parseInt(c.id) === parseInt(condition.id)) > -1}
                             /> {condition.desc}
                         </label>
                     ))}
                 </div>
-            <input className="big-button center" type="submit" />
+            <input className="big-button center" type="submit" disabled={isDisabled}/>
         </form>
+    </>
     )
 }
 
--- src/components/activity/changeFunctions.js
+++ src/components/activity/changeFunctions.js
@@ -1,18 +1,46 @@
 
+export function validateField(name, value){
+    let message = '';
+
+    switch (name){
+        case 'desc':
+            (value.match(/\S/)) ? message = '' : message = "Description cannot be blank."
+            this.setState({formErrors: {...this.state.formErrors, desc: message}});
+            break;
+        case 'min_temp':
+            ((value === '' || value.match(/^[0-9]*$/))) && (value >-1 && value <101) ? message = '' : message = "Min. Temp must be a number between 0 and 100."
+            this.setState({formErrors: {...this.state.formErrors, min_temp: message}});
+            break;
+        case 'max_temp':        
+        ((value === '' || value.match(/^[0-9]*$/))) && (value >-1 && value < 101) ? message = '' : message = "Max. Temp must be a number between 0 and 100."
+            this.setState({formErrors: {...this.state.formErrors, max_temp: message}});
+            break;
+        case 'max_wind_speed':
+            (value.match(/^[0-9]*$/)) && ((value >= 1 && value < 51) || value === '') ? message = '' : message = "Max. Wind Speed must be a number between 1 and 50."
+            this.setState({formErrors: {...this.state.formErrors, max_wind_speed: message}});
+            break;
+        default:
+            break;
+    }
+}
+
 export function handleActivityChange(event){
-   this.setState({
-        activity: {...this.state.activity, [event.target.name]: event.target.value }
-    })
+    const name = event.target.name;
+    const value = event.target.value ;
+    this.setState({
+            activity: {...this.state.activity, [name]: value }
+        }, 
+        () => {this.validateField(name, value)})
 }
 
 export function handleConditionSelection(event) {
-    const clickedCondition = this.props.conditions.find(c=> c.id == event.target.value);
+    const clickedCondition = this.props.conditions.find(c=> parseInt(c.id) === parseInt(event.target.value));
     let conditionArray;
     // if the id provided by the selection is found in the state array of activity conditions
     // then remove the condition from the state array.
     // else add the selection to the state array.
-    if(this.state.activity.conditions.findIndex(c => c.id == clickedCondition.id) > -1) {
-        conditionArray = this.state.activity.conditions.filter(c => c.id != clickedCondition.id)
+    if(this.state.activity.conditions.findIndex(c => parseInt(c.id) === parseInt(clickedCondition.id)) > -1) {
+        conditionArray = this.state.activity.conditions.filter(c => parseInt(c.id) !== parseInt(clickedCondition.id))
     } else {
         conditionArray = [...this.state.activity.conditions, clickedCondition] 
     }
