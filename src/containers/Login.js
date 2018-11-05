305a05f2be55d016262e725f04ea01fa9dbdf5ad
BrownCow371 <jmbrowncow@gmail.com>
2018-11-05 08:27:09 -0800

    add Login Container

---
+++ src/containers/Login.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {loginUser} from '../actions/actions.js';
+
+
+class Login extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            auth: {
+                email: '',
+                password: ''
+            }
+        }
+    }
+
+    handleSubmit =(event) => {
+        event.preventDefault();
+        this.props.loginUser(this.state)
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            auth: {...this.state.auth, 
+            [event.target.name]: event.target.value}
+        })
+
+    }
+
+    render(){
+        return (
+            <form>
+                <label><strong> Email: </strong></label>
+                <input type="email" name="email" value={this.state.auth.email} onChange={this.handleChange}/>
+                <br />
+                <label><strong> Password: </strong></label>
+                <input type="password" name="password" value={this.state.auth.password} onChange={this.handleChange}/>
+                <br />
+                <input type="submit"  onClick={this.handleSubmit}/>
+            </form>
+        )
+    }
+}
+
+export default connect(null, {loginUser})(Login)
