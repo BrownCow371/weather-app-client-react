a833e8373563e3d289880810977d1c9d234ebeb8
BrownCow371 <jmbrowncow@gmail.com>
2018-11-04 20:44:00 -0800

    change render to redirect in internal state. remove commented out fetch calls in component did mount. add error handling to handlesubmit function. update render to not include loading logic - to be handled by show container. add errMessage to mapStatetoProps from store. add header to render to show error if exists

--- src/containers/EditActivity.js
+++ src/containers/EditActivity.js
@@ -3,14 +3,14 @@ import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
 import {updateActivity} from '../actions/actions.js';
 import ActivityForm from '../components/activity/ActivityForm.js';
-import Loading from '../components/static/Loading.js'
+// import Loading from '../components/static/Loading.js'
 import {handleActivityChange, handleConditionSelection, validateField} from '../components/activity/changeFunctions.js'
 
 class EditActivity extends Component {
     constructor(props){
         super(props)
         this.state = {
-            render: "EDIT_FORM",
+            redirect: "EDIT_FORM",
             activity: {
                 id: '',
                 desc: '',
@@ -36,57 +36,52 @@ class EditActivity extends Component {
         if (activity) {
             this.setState({activity: activity}) 
         } else {
-            this.setState({render: 'REDIRECT'})
+            this.setState({redirect: 'REDIRECT'})
         }
-        
-        // fetch(`/api/activities/${id}`)
-        //     .then(response => response.json())
-        //     .then(activity => this.setState({activity: activity}))
-
-        // fetch(`/api/conditions`)
-        //     .then(response => response.json())
-        //     .then(conditions => this.setState({conditions: conditions}))
     }
 
 
     handleSubmit = (event) => {
         event.preventDefault();
         this.props.updateActivity(this.state)
-        this.setState({render: 'REDIRECT_SHOW'})
+        .then(action => {
+            console.log("edit action", action)
+            if(action) {
+                this.setState({redirect: 'REDIRECT_SHOW'})
+            }
+        })  
     }
 
+
     render(){
-        if (this.props.loading) {
-            return <Loading />
-        } else {
-            switch(this.state.render){
-                case 'REDIRECT_SHOW':
-                    return <Redirect to={`/activities/${this.state.activity.id}`}/>
-                case 'REDIRECT':
-                    return <Redirect to="/activities"/>
-                default:
-                    return (
-                        <div className="activity-box">
-                            <ActivityForm 
-                                formErrors={this.state.formErrors}
-                                activity={this.state.activity}
-                                conditions={this.props.conditions} 
-                                handleSubmit={this.handleSubmit}
-                                handleChange={this.handleActivityChange}
-                                handleCheckbox={this.handleConditionSelection}
-                                title={`Edit Activity ID: ${this.state.activity.id} Form`}/>
-                        </div>
-                )
-            } 
-         }
-    }  
+        switch(this.state.redirect){
+            case 'REDIRECT_SHOW':
+                return <Redirect to={`/activities/${this.state.activity.id}`}/>
+            case 'REDIRECT':
+                return <Redirect to="/activities"/>
+            default:
+                return (
+                    <div className="activity-box">
+                        <ActivityForm 
+                            formErrors={this.state.formErrors}
+                            activity={this.state.activity}
+                            conditions={this.props.conditions} 
+                            handleSubmit={this.handleSubmit}
+                            handleChange={this.handleActivityChange}
+                            handleCheckbox={this.handleConditionSelection}
+                            title={`Edit Activity ID: ${this.state.activity.id} Form`}/>
+                        <h3 className="warning">{this.props.errMessage}</h3>
+                    </div>
+            )
+        } 
+    }
      
 }
 
 const mapStateToProps= (state) => {
     return {activities: state.activities,
             conditions: state.conditions,
-            loading: state.loading}
+            errMessage: state.errMessages.activityError}
 }
 
 export default connect(mapStateToProps, {updateActivity})(EditActivity)
