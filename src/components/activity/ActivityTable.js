7eeb46f960bfeea3e8156dd19e38f36a78ad7880
BrownCow371 <jmbrowncow@gmail.com>
2018-11-07 14:21:33 -0800

    change ActivityTablet o a class in order to hold state. add Sort List button to sort list by likes. set up state to hold sorted list. set up sortListClick function to sort a copy of props and store in state. add activityList variable to render to be used by map method of ActivityRows

--- src/components/activity/ActivityTable.js
+++ src/components/activity/ActivityTable.js
@@ -1,38 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 import ActivityRow from './ActivityRow.js'
 
-const ActivityTable = (props) => {
+class ActivityTable extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            activities: [],
+            useState: false
+        }
+    }
+    
+    sortListClick = (event) => {
+        event.preventDefault();
+        this.setState({
+            activities: [...this.props.activities].sort(
+                (activityA, activityB)=> (activityB.likes - activityA.likes)),
+            useState: true
+            })
+    }
 
-    return (
-        <div className="activities-box">
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Id</th>
-                        <th>Desc</th>
-                        <th>Min Temp</th>
-                        <th>Max Temp</th>
-                        <th>Max Wind Speed</th>
-                        <th>Acceptable Conditions</th>
-                        <th>Edit</th>
-                        <th>Remove</th>
-                        <th>Like</th>
-                        <th>Like Count</th>
-                    </tr>
-                    {props.activities.map((activity) => (
-                        <ActivityRow 
-                            key={activity.id}
-                            activity={activity}
-                            handleClickRemove={props.handleClickRemove}
-                            />
-                    ))}
-                </tbody>
-            </table>
-            <button className="big-button" ><Link to ="/activities/new">Add New Activity</Link> </button>          
-        </div>
-    )
+    render(){
+        let activityList = this.props.activities;
+        
+        if (this.state.useState) {
+            activityList = this.state.activities
+        } 
+
+        return (
+            <div className="activities-box">
+            <button className="big-button" onClick={this.sortListClick}> Sort by Likes </button>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Id</th>
+                            <th>Desc</th>
+                            <th>Min Temp</th>
+                            <th>Max Temp</th>
+                            <th>Max Wind Speed</th>
+                            <th>Acceptable Conditions</th>
+                            <th>Edit</th>
+                            <th>Remove</th>
+                            <th>Like</th>
+                            <th>Like Count</th>
+                        </tr>
+                        {activityList.map((activity) => (
+                            <ActivityRow 
+                                key={activity.id}
+                                activity={activity}
+                                handleClickRemove={this.props.handleClickRemove}
+                                />
+                        ))}
+                    </tbody>
+                </table>
+                <button className="big-button" ><Link to ="/activities/new">Add New Activity</Link> </button>          
+            </div>
+            )
+    }   
 }
 
 export default ActivityTable;
