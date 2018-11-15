66274edab7015e002a18dabe81e133c787b2d33e
BrownCow371 <jmbrowncow@gmail.com>
2018-11-15 15:01:28 -0800

    add case for Log Out. Add case for Login Success. Update Login Error to use lovercase u in userError. Change Logging in User to Lggin User

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,11 +1,13 @@
 export default function usersReducer(state,action){
     switch(action.type){
-         case 'LOGGING_IN_USER':
+        case 'LOGGING_USER':
             return {...state, authenticating: true, errMessages: {}}
-        case 'LOGIN_USER':
-            return {...state, jwt: action.payload, authenticating: false}
-        case  'LOGIN_ERROR':
-            return {...state, suggestion: {}, errMessages: {...state.errMessages, UserError: action.payload.error}, loading: false}
+        case 'LOGIN_SUCCESS':
+            return {...state, session: true, authenticating: false}
+        case 'LOG_OUT':
+            return {...state, session: false}
+        case 'LOGIN_ERROR':
+            return {...state, errMessages: {...state.errMessages, userError: action.payload.error}, loading: false}
         default:
             return state;
     }
