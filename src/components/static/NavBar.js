68b4b2766b438112c3a6f5858ae6b4b92436223c
BrownCow371 <jmbrowncow@gmail.com>
2018-11-15 16:03:18 -0800

    update class on home button to be first big button

--- src/components/static/NavBar.js
+++ src/components/static/NavBar.js
@@ -17,7 +17,7 @@ class NavBar extends Component {
     render () {
         return (
             <div className="navbar clearfix">
-                <button className="big-button"><NavLink to ="/" exact>Home</NavLink> </button>
+                <button className="first-big-button"><NavLink to ="/" exact>Home</NavLink> </button>
                 {this.Logged()}
                 <button className="big-button"><NavLink to ="/weather" exact>Weather Search</NavLink> </button> 
                 <button className="big-button"><NavLink to ="/activities" exact>Activity List</NavLink> </button> 
