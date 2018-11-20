6f301cb9f8753979345c09ad4e7d17e78a2e8c20
BrownCow371 <jmbrowncow@gmail.com>
2018-11-20 12:39:05 -0800

    sessionStorage must be asynchronous - not properly setting session. switched to true or false

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -6,11 +6,11 @@ export default function usersReducer(state,action){
             // console.log("jwt login", sessionStorage.jwt)
             // need to force window reload in order for sessionStorage to be updated in browser
             window.location.reload().then(response => {
-                return {...state, session: !!sessionStorage.jwt, authenticating: false}}
-            )         
+                return {...state, session: true, authenticating: false}
+            })         
         case 'LOG_OUT':
             // console.log("jwt logout", sessionStorage.jwt)
-            return {...state, session: !!sessionStorage.jwt}
+            return {...state, session: false}
         case 'LOGIN_ERROR':
             return {...state, errMessages: {...state.errMessages, loginError: action.payload.error}, loading: false}
         case 'USER_ERROR':
