c738fe8d74d1539f911949186429d24248bc67c6
BrownCow371 <jmbrowncow@gmail.com>
2018-11-20 11:59:14 -0800

    connect greating to store so that it re-renders with user name when user loggedin

--- src/components/static/Greeting.js
+++ src/components/static/Greeting.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-const Greeting = () => {
+class Greeting extends Component {
+    // needs to be connected to the store so that the User Name is rendered when log in or out
+    render(){
 
+        if (sessionStorage.user) {
+            return (
+                <div>
+                <h1 className="center">Welcome <span className="blue">{sessionStorage.user}!</span>, to the Weather Based Activity App!</h1>
+            </div>
+            )
+        } else {
          return(
             <div>
                 <h1 className="center">Welcome to the Weather Based Activity App!</h1>
             </div>
          )
+        }
+    }
 }
 
-export default Greeting
+const mapStateToProps = (state) => {
+    return {logged_in: state.session}
+}
+
+export default connect(null)(Greeting)
