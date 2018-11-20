adc0278cb618b02f59f423b00c4e5556575271eb
BrownCow371 <jmbrowncow@gmail.com>
2018-11-20 11:23:42 -0800

    create header creation functions to include authorization jwt

---
+++ src/actions/headers.js
@@ -0,0 +1,15 @@
+
+function authHeader() {
+    // if (sessionStorage.getItem('jwt')){
+        return {'AUTHORIZATION': `Bearer ${sessionStorage.getItem('jwt')}`}
+        // }
+}
+
+export const reqHeaders = Object.assign({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'},
+    authHeader()
+    )
+
+
+
