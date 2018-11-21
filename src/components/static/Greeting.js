1d4fa7b6056706da93b84d12190a81d05c94b068
BrownCow371 <jmbrowncow@gmail.com>
2018-11-21 09:27:37 -0800

    uncomment mapStateToProps, use logged_in props to determine rendering. comment out testing code

--- src/components/static/Greeting.js
+++ src/components/static/Greeting.js
@@ -5,27 +5,28 @@ class Greeting extends Component {
     // needs to be connected to the store so that the User Name is rendered when log in or out
     render(){
 
-        if (!!sessionStorage.user) {
-        // if (this.props.logged_in) {
+        // let sessionStat = (this.props.logged_in ? "TRUE" : "FALSE")
+        // <p>Session Status: {sessionStat}</p>
+        // <p>Session jwt: {sessionStorage.jwt}</p>
+
+        if (this.props.logged_in) {
             return (
                 <div>
-                <h1 className="center">Welcome <span className="blue">{sessionStorage.user}!</span>, to the Weather Based Activity App!</h1>
-            </div>
+                    <h1 className="center">Welcome <span className="blue">{sessionStorage.user}!</span>, to the Weather Based Activity App!</h1>
+                </div>
             )
         } else {
-         return(
-            <div>
-                <h1 className="center">Welcome to the Weather Based Activity App!</h1>
-            </div>
-         )
+            return(
+                <div>
+                    <h1 className="center">Welcome to the Weather Based Activity App!</h1>
+                </div>
+            )
         }
     }
 }
 
-// const mapStateToProps = (state) => {
-//     return {logged_in: state.session}
-// }
+const mapStateToProps = (state) => {
+    return {logged_in: state.session}
+}
 
-// export default connect(mapStateToProps)(Greeting)
-
-export default connect(null)(Greeting)
+export default connect(mapStateToProps)(Greeting)
