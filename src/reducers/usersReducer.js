541f6540b12c5dff560454209829f2f16c17283a
BrownCow371 <jmbrowncow@gmail.com>
2018-11-21 09:42:26 -0800

    remove console logging, change wuthenticating to loading, remove window.reload.

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,16 +1,11 @@
 export default function usersReducer(state,action){
     switch(action.type){
         case 'LOGGING_USER':
-            return {...state, authenticating: true, errMessages: {}}
+            return {...state, loading: true, errMessages: {}}
         case 'LOGIN_SUCCESS':
-            // console.log("jwt login", sessionStorage.jwt)
-            // need to force window reload in order for sessionStorage to be updated in browser
-            window.location.reload().then(response => {
-                return {...state, session: true, authenticating: false}
-            })         
+            return {...state, session: !!sessionStorage.jwt, loading: false}
         case 'LOG_OUT':
-            // console.log("jwt logout", sessionStorage.jwt)
-            return {...state, session: false}
+            return {...state, session: !!sessionStorage.jwt}
         case 'LOGIN_ERROR':
             return {...state, errMessages: {...state.errMessages, loginError: action.payload.error}, loading: false}
         case 'USER_ERROR':
